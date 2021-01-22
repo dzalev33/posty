4015ccdd424e78b7d7a781475dc1c865cc39dd07
Dzalev <dzalev@hotmail.com>
2021-01-22 19:05:38 +0100

    likes and dislikes

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,7 +5,7 @@
         <div class="w-8/12 bg-white p-6 rounded-lg">
             <form action="{{ route('posts') }}" method="POST" class="mb-4">
                 @csrf
-                <div class="mb-4">
+                <div class="mb-6">
                     <label for="body" class="sr-only">Body</label>
                     <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100"
                               placeholder="Post Something!"></textarea>
@@ -29,6 +29,24 @@
                         <a href="" class="font-bold">{{ $post->user->name }}</a>
                         <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
                         <p class="mb-2">{{ $post->body }}</p>
+
+                        <div class="flex items-center">
+                            @if(!$post->likedBy(auth()->user()))
+
+                            <form action="{{ route( 'posts.likes',$post )}}" method="post" class="mr-1">
+                                @csrf
+                                <button type="submit" class="text-blue-500">Like</button>
+                            </form>
+                            @else
+                            <form action="{{ route( 'posts.likes',$post )}}" method="post" class="mr-1">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="text-blue-500">Unlike</button>
+                            </form>
+                            @endif
+                            <span>{{ $post->likes->count() }}
+                                {{ Str::plural('like',$post->likes->count()) }}</span>
+                        </div>
                     </div>
                 @endforeach
                 {{ $posts->links() }}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\PostLikeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 
@@ -36,6 +37,9 @@ Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/posts', [PostController::class, 'index'])->name('posts');
 Route::post('/posts', [PostController::class, 'store']);
+
+Route::post('/posts/{post}/likes', [PostLikeController::class, 'store'])->name('posts.likes');
+Route::delete('/posts/{post}/likes', [PostLikeController::class, 'destroy'])->name('posts.likes');
 
 
 
