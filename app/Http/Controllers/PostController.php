b0584319c3d8ea7b83317ce2c8b607fdae951532
Dzalev <dzalev@hotmail.com>
2021-01-24 00:45:54 +0100

    eager loading

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::paginate(20);
+        $posts = Post::latest()->with(['user', 'likes'])->paginate(20);
         return view('posts.index',[
             'posts' => $posts
         ]);
@@ -24,5 +24,12 @@ class PostController extends Controller
        $request->user()->posts()->create($request->only('body'));
 
        return back();
+    }
+
+    public function destroy(Post $post){
+
+        $this->authorize('delete', $post);
+        $post->delete();
+        return back();
     }
 }
